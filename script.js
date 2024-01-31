64274dd772282167a30a9c516befe73584bbb7f0
szidimisi68 <70961855+szidimisi68@users.noreply.github.com>
2024-01-31 14:39:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,300 @@
+let adatok = [
+    {
+      "OM_Azonosito": "78655218932",
+      "Neve": "Szabó Anna",
+      "ErtesitesiCime": "Budapest, Gellért tér 15.",
+      "Email": "anna@example.com",
+      "SzuletesiDatum": "1998-07-19T00:00:00",
+      "Matematika": 14,
+      "Magyar": 35
+    },
+    {
+      "OM_Azonosito": "15963702584",
+      "Neve": "Nagy Zsófia",
+      "ErtesitesiCime": "Debrecen, Szent István utca 8.",
+      "Email": "zsofia@example.com",
+      "SzuletesiDatum": "2000-02-22T00:00:00",
+      "Matematika": 27,
+      "Magyar": 4
+    },
+    {
+      "OM_Azonosito": "30351479261",
+      "Neve": "Kovács Máté",
+      "ErtesitesiCime": "Szeged, Erzsébet körút 45.",
+      "Email": "mate@example.com",
+      "SzuletesiDatum": "1995-11-29T00:00:00",
+      "Matematika": 48,
+      "Magyar": 15
+    },
+    {
+      "OM_Azonosito": "97401028543",
+      "Neve": "Tóth Bence",
+      "ErtesitesiCime": "Pécs, Váci utca 33.",
+      "Email": "bence@example.com",
+      "SzuletesiDatum": "1997-03-17T00:00:00",
+      "Matematika": 8,
+      "Magyar": 47
+    },
+    {
+      "OM_Azonosito": "88765031624",
+      "Neve": "Horváth Eszter",
+      "ErtesitesiCime": "Székesfehérvár, Bartók Béla út 12.",
+      "Email": "eszter@example.com",
+      "SzuletesiDatum": "1996-09-08T00:00:00",
+      "Matematika": 34,
+      "Magyar": 7
+    },
+    {
+      "OM_Azonosito": "64189075351",
+      "Neve": "Kiss Attila",
+      "ErtesitesiCime": "Miskolc, József Attila utca 18.",
+      "Email": "attila@example.com",
+      "SzuletesiDatum": "1993-12-05T00:00:00",
+      "Matematika": 13,
+      "Magyar": 48
+    },
+    {
+      "OM_Azonosito": "18734250658",
+      "Neve": "Fekete Laura",
+      "ErtesitesiCime": "Győr, Széchenyi tér 9.",
+      "Email": "laura@example.com",
+      "SzuletesiDatum": "1999-06-30T00:00:00",
+      "Matematika": 2,
+      "Magyar": 30
+    },
+    {
+      "OM_Azonosito": "51698072427",
+      "Neve": "Bíró Gábor",
+      "ErtesitesiCime": "Kecskemét, Deák Ferenc utca 21.",
+      "Email": "gabor@example.com",
+      "SzuletesiDatum": "1994-10-14T00:00:00",
+      "Matematika": 9,
+      "Magyar": 33
+    },
+    {
+      "OM_Azonosito": "60157349268",
+      "Neve": "Mészáros Péter",
+      "ErtesitesiCime": "Nyíregyháza, Petőfi Sándor utca 26.",
+      "Email": "peter@example.com",
+      "SzuletesiDatum": "2001-04-01T00:00:00",
+      "Matematika": 36,
+      "Magyar": 21
+    },
+    {
+      "OM_Azonosito": "72948316750",
+      "Neve": "Varga Noémi",
+      "ErtesitesiCime": "Szombathely, Kossuth Lajos utca 3.",
+      "Email": "noemi@example.com",
+      "SzuletesiDatum": "1992-08-18T00:00:00",
+      "Matematika": 24,
+      "Magyar": 23
+    },
+    {
+      "OM_Azonosito": "84052731649",
+      "Neve": "Lakatos Dóra",
+      "ErtesitesiCime": "Veszprém, Ady Endre utca 7.",
+      "Email": "dora@example.com",
+      "SzuletesiDatum": "2000-01-03T00:00:00",
+      "Matematika": 43,
+      "Magyar": 41
+    },
+    {
+      "OM_Azonosito": "85273941680",
+      "Neve": "Németh Tamás",
+      "ErtesitesiCime": "Szolnok, Béke tér 14.",
+      "Email": "tamas@example.com",
+      "SzuletesiDatum": "1998-05-27T00:00:00",
+      "Matematika": 5,
+      "Magyar": 49
+    },
+    {
+      "OM_Azonosito": "41593260701",
+      "Neve": "Orbán Katalin",
+      "ErtesitesiCime": "Eger, Szabadság utca 32.",
+      "Email": "katalin@example.com",
+      "SzuletesiDatum": "1996-02-11T00:00:00",
+      "Matematika": 37,
+      "Magyar": 20
+    },
+    {
+      "OM_Azonosito": "10486732952",
+      "Neve": "Simon Balázs",
+      "ErtesitesiCime": "Debrecen, Király utca 28.",
+      "Email": "balazs@example.com",
+      "SzuletesiDatum": "1995-07-07T00:00:00",
+      "Matematika": 20,
+      "Magyar": 48
+    },
+    {
+      "OM_Azonosito": "92740581643",
+      "Neve": "Papp Viktória",
+      "ErtesitesiCime": "Kaposvár, Alkotmány utca 5.",
+      "Email": "viktor@example.com",
+      "SzuletesiDatum": "1997-11-24T00:00:00",
+      "Matematika": 32,
+      "Magyar": 9
+    },
+    {
+      "OM_Azonosito": "10637851454",
+      "Neve": "Molnár Zoltán",
+      "ErtesitesiCime": "Szekszárd, Párizsi utca 17.",
+      "Email": "zoltan@example.com",
+      "SzuletesiDatum": "1993-01-16T00:00:00",
+      "Matematika": 3,
+      "Magyar": 46
+    },
+    {
+      "OM_Azonosito": "44025967885",
+      "Neve": "Fekete Márton",
+      "ErtesitesiCime": "Pécs, Rákóczi út 13.",
+      "Email": "marton@example.com",
+      "SzuletesiDatum": "1992-04-29T00:00:00",
+      "Matematika": 42,
+      "Magyar": 31
+    },
+    {
+      "OM_Azonosito": "30381425616",
+      "Neve": "Pál Júlia",
+      "ErtesitesiCime": "Sopron, Szent Gellért tér 10.",
+      "Email": "julia@example.com",
+      "SzuletesiDatum": "1999-09-02T00:00:00",
+      "Matematika": 49,
+      "Magyar": 19
+    },
+    {
+      "OM_Azonosito": "65082317920",
+      "Neve": "Takács Orsolya",
+      "ErtesitesiCime": "Eger, Andrássy út 22.",
+      "Email": "orsolya@example.com",
+      "SzuletesiDatum": "1994-06-13T00:00:00",
+      "Matematika": 31,
+      "Magyar": 18
+    },
+    {
+      "OM_Azonosito": "15374680221",
+      "Neve": "Kovács Ádám",
+      "ErtesitesiCime": "Székesfehérvár, Bajnai út 8.",
+      "Email": "adam@example.com",
+      "SzuletesiDatum": "1996-08-06T00:00:00",
+      "Matematika": 18,
+      "Magyar": 10
+    }
+  ]
+
+
+function Keres()
+{
+    let osszPont = 0;
+    let talalat = false
+    let numberRegex = /^\d+$/;  
+    let azon = document.getElementById("OM").value
+    let csere = true
+    document.getElementById("talalat").innerHTML = ""
+    document.getElementById("tobbPontosok").innerText = ""
+    document.getElementById("talalatTabla1").innerHTML = ""
+    document.getElementById("talalatTabla2").innerHTML = ""
+    if (azon.length != 11) {
+        document.getElementById("auth").innerText = "Sikertelen keresés"
+        document.getElementById("auth").classList = "sikertelen"
+        alert("Az azonosító nem 11 karakter hosszú")
+    }
+    else if (!numberRegex.test(azon)) {
+        document.getElementById("auth").innerText = "Sikertelen keresés"
+        document.getElementById("auth").classList = "sikertelen"
+        alert("Az OM azonosító csak számokból állhat")
+    }
+    else{
+        for (let adat of adatok) {
+            console.log(adat)
+            if (adat.OM_Azonosito == azon) {
+                document.getElementById("talalat").innerHTML =
+                `
+                  <div class="sor">
+                    <div class="cim">Email:</div>
+                    <div class="adat">${adat.Email}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">Értesítési Címe:</div>
+                    <div class="adat">${adat.ErtesitesiCime}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">Magyar:</div>
+                    <div class="adat">${adat.Magyar}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">Matekmatika:</div>
+                    <div class="adat">${adat.Matematika}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">Neve:</div>
+                    <div class="adat">${adat.Neve}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">OM Azonosító:</div>
+                    <div class="adat">${adat.OM_Azonosito}</div>
+                  </div>
+                  <div class="sor">
+                    <div class="cim">Születési dátum:</div>
+                    <div class="adat">${adat.SzuletesiDatum}</div>
+                  </div>
+                `
+                osszPont = adat.Matematika + adat.Magyar
+                talalat = true
+                break
+            }
+        }
+        
+        if (talalat) {
+          let tobbetElerokSzama = 0
+            document.getElementById("auth").classList = "sikeres"
+            document.getElementById("auth").innerText = "Sikeres találat"
+            console.log("asd")
+            document.getElementById("talalatTabla1").innerHTML +=
+            `
+              <tr>
+                <th>OMaz.:</th>
+                <th>Neve:</th>
+              </tr>
+            `
+            document.getElementById("talalatTabla2").innerHTML +=
+            `
+              <tr>
+                <th>OMaz.:</th>
+                <th>Neve:</th>
+              </tr>
+            `
+            for (let adat of adatok) {
+              if (adat.Matematika + adat.Magyar > osszPont) {
+                if (csere) {
+                  document.getElementById("talalatTabla1").innerHTML +=
+                  `
+                    <tr>
+                      <td>${adat.OM_Azonosito}</td>
+                      <td>${adat.Neve}</td>
+                    </tr>
+                  `
+                  csere = false
+                }
+                else{
+                  document.getElementById("talalatTabla2").innerHTML +=
+                  `
+                    <tr>
+                      <td>${adat.OM_Azonosito}</td>
+                      <td>${adat.Neve}</td>
+                    </tr>
+                  `
+                  csere = true
+                }
+                tobbetElerokSzama++;
+              }
+            }
+            document.getElementById("tobbPontosok").innerText = "Több pontot elérő diákok száma: " + tobbetElerokSzama
+            
+          }
+        else{
+            document.getElementById("auth").classList = "sikertelen"
+            document.getElementById("auth").innerText = "Nincs találat"
+        }
+
+    }
+}
